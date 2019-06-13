cc1d4d0edd2ea5a59c237207e8d49c9d7d235ac2
omidshj <omid.shj@gmail.com>
2019-06-13 12:19:52 +0430

    create invoice, and tag sketch

---
+++ database/migrations/2019_06_13_074013_create_tags_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->unsignedInteger('tag_id')->index();
+            $table->char('taggable_type', 100);
+            $table->unsignedInteger('taggable_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
